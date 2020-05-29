84555a9424010509a087eac88d8d897da249ae7f
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-05-29 18:29:53 +0530

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,2 +1,2 @@
-export const API_KEY = "89235e4ac324401b8a56bd83de158669";
+export const API_KEY = "*******************************"; // your API key of openweathermap goes here
 export const BASE_URL = "api.openweathermap.org/data/2.5/forecast";
